adabbd276d1069150d889698afc1eb3351b2c5c8
AUTO ONE <youssefelnahhry546@gmail.com>
2026-09-12 14:10:53 +0300

    رجوع الكارت لاصلو

--- auto_one_pro/lib/main.dart
+++ auto_one_pro/lib/main.dart
@@ -307,7 +307,6 @@ class _AutoOneShellState extends State<AutoOneShell> {
 
       child: Scaffold(
         backgroundColor: const Color(0xfff5f5f5),
-        extendBodyBehindAppBar: true,
 
         // ======================================================
         // TOP BAR
@@ -319,7 +318,6 @@ class _AutoOneShellState extends State<AutoOneShell> {
           child: AutoOneHeader(
             isArabic: widget.isArabic,
             showCars: showCars,
-            transparent: !showCars,
 
             onHome: openHome,
            onCars: () => openCars(),
